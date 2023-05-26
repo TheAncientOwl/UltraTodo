30656d82a5e8dde1e5fe9ec81f3ee5d4726bf186
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-05-26 20:14:24 +0300

    tweaks

--- app/src/main/java/ro/ase/pdm/ultratodo/ui/alltodos/AllTodosFragment.kt
+++ app/src/main/java/ro/ase/pdm/ultratodo/ui/alltodos/AllTodosFragment.kt
@@ -1,4 +1,4 @@
-package ro.ase.pdm.ultratodo.ui.gallery
+package ro.ase.pdm.ultratodo.ui.alltodos
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ro.ase.pdm.ultratodo.databinding.FragmentGalleryBinding
+import ro.ase.pdm.ultratodo.databinding.FragmentAllTodosBinding
 
-class GalleryFragment : Fragment() {
+class AllTodosFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentAllTodosBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val allTodosViewModel =
+            ViewModelProvider(this).get(AllTodosViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAllTodosBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        allTodosViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/ro/ase/pdm/ultratodo/ui/alltodos/AllTodosViewModel.kt
+++ app/src/main/java/ro/ase/pdm/ultratodo/ui/alltodos/AllTodosViewModel.kt
@@ -1,13 +1,13 @@
-package ro.ase.pdm.ultratodo.ui.gallery
+package ro.ase.pdm.ultratodo.ui.alltodos
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class AllTodosViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is all todos Fragment"
     }
     val text: LiveData<String> = _text
 }
