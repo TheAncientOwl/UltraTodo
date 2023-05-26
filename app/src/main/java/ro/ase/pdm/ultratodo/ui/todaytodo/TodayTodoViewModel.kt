0e257f309a1c57f386b9c9747252b73cfa419494
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-05-26 20:04:52 +0300

    tweaks

--- app/src/main/java/ro/ase/pdm/ultratodo/ui/todaytodo/TodayTodoViewModel.kt
+++ app/src/main/java/ro/ase/pdm/ultratodo/ui/todaytodo/TodayTodoViewModel.kt
@@ -1,13 +1,13 @@
-package ro.ase.pdm.ultratodo.ui.home
+package ro.ase.pdm.ultratodo.ui.todaytodo
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class TodayTodoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is today todo Fragment"
     }
     val text: LiveData<String> = _text
 }
