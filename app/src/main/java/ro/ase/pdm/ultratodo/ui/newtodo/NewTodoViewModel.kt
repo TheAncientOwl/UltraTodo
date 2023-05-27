5bd8bd3d4ab88ed5163b16ad950fd2664302f05b
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-05-27 15:08:51 +0300

    ok

--- app/src/main/java/ro/ase/pdm/ultratodo/ui/newtodo/NewTodoViewModel.kt
+++ app/src/main/java/ro/ase/pdm/ultratodo/ui/newtodo/NewTodoViewModel.kt
@@ -2,12 +2,27 @@ package ro.ase.pdm.ultratodo.ui.newtodo
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import ro.ase.pdm.ultratodo.data.Todo
+import ro.ase.pdm.ultratodo.data.*
 
 class NewTodoViewModel : ViewModel() {
-    fun saveTodo(todo: Todo) {
-        // TODO: implement
+    fun saveTodo(
+        title: String,
+        description: String,
+        priority: TodoPriority,
+        type: TodoType,
+        location: Location?
+    ) {
+        val todo = Todo(
+            title,
+            priority,
+            TodoState.Pending,
+            type,
+            description,
+            location
+        )
+
+        // TODO: save it to database
         Log.d("ULTRA-TODO", "SHOULD SAVE")
-        Log.d("ULTRA-TODO", todo.title + " - " + todo.priority.toString() + todo.type.toString())
+        Log.d("ULTRA-TODO", todo.title + " - " + todo.priority.toString() + " - " + todo.type.toString())
     }
 }
