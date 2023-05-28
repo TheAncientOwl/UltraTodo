60c291d7215d7d157e572624c6858614e3b02321
gabiciprian24 <cipriangoia14@gmail.com>
2023-05-28 13:25:52 +0300

    OK OK OK

--- app/src/main/java/ro/ase/pdm/ultratodo/MainActivity.kt
+++ app/src/main/java/ro/ase/pdm/ultratodo/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_todo_list, R.id.nav_new_todo
+                R.id.nav_todo_list, R.id.nav_new_todo, R.id.aboutTodoFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -48,6 +48,9 @@ class MainActivity : AppCompatActivity() {
         binding.appBarMain.fab.setOnClickListener {
             navController.navigate(R.id.nav_new_todo)
         }
+
+        // Navigate to the AboutTodoFragment when the app starts
+        navController.navigate(R.id.aboutTodoFragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
