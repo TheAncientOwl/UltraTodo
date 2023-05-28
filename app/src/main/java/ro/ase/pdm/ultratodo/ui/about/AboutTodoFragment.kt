6a86241f00b15041b4426cdea2ce8fb4268fdd65
Bita121 <francu352000@gmail.com>
2023-05-29 01:58:16 +0300

    test2

--- app/src/main/java/ro/ase/pdm/ultratodo/ui/about/AboutTodoFragment.kt
+++ app/src/main/java/ro/ase/pdm/ultratodo/ui/about/AboutTodoFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import ro.ase.pdm.ultratodo.R
 
 class AboutTodoFragment : Fragment() {
@@ -20,7 +22,10 @@ class AboutTodoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_about_todo, container, false)
-
+        val buttonMore: Button = rootView.findViewById(R.id.navToInfoButton)
+        buttonMore.setOnClickListener{
+            findNavController().navigate(R.id.InfoTodoFragment)
+        }
         val appNameTextView: TextView = rootView.findViewById(R.id.text_app_name)
         val versionTextView: TextView = rootView.findViewById(R.id.text_version)
         val authorsTextView: TextView = rootView.findViewById(R.id.text_authors)
